f5ccb704b8d928a2006ec9cd9ff3e3eda931e7d7
Jonietech <jonahbst@gmail.com>
2025-04-30 14:46:12 +0300

    Update .gitignore and add test instructions

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# make sure to pip install pytest before running the tests
+# you can run all the tests by running "pytest tests/" in your terminal
+# or you can run a specific test file by running "pytest tests/test_file.py"
